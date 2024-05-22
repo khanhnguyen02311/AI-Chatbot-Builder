cd0b7ce290d79c6de69ce90bdf5e83501233eec0
Khanh Nguyen <khanhnguyen02311@gmail.com>
2024-05-22 10:58:30 +0700

    fix: refactor envs & runtime arguments location

--- backend/configurations/arguments.py
+++ backend/configurations/arguments.py
@@ -1,8 +1,9 @@
 import argparse
-from dotenv import load_dotenv
 
-stage = None
-debug = None
+# Global variables to store the parsed arguments from main
+APP_STAGE = "dev"
+APP_DEBUG = True
+APP_API_PORT = 8000
 
 
 def str2bool(v):
@@ -17,20 +18,18 @@ def str2bool(v):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--stage", help="specify the development stage (dev/staging/prod)", type=str, default="dev")
-    parser.add_argument("--debug", help="application debug mode (True/False), default: True", type=str2bool, nargs="?", const=True, default=False)
+    parser.add_argument("--stage", help="specify the development stage (dev/staging/prod). Default: dev", type=str, default="dev")
+    parser.add_argument("--debug", help="application debug mode (True/False), default: True", type=str2bool, default=True)
+    parser.add_argument("--port", help="specify the port number for the API server", type=int, default=8000)
     args = parser.parse_args()
 
     if args.stage not in ["dev", "staging", "prod"]:
-        parser.error("--stage: Invalid argument value (dev/staging/prod).")
+        raise argparse.ArgumentTypeError("Invalid stage. Please specify one of the following: dev, staging, prod")
 
-    load_dotenv(dotenv_path=f'.env.{args.stage}', verbose=True)
-
-    # print(args.stage, args.debug)
-
-    global stage
-    global debug
-    stage = args.stage
-    debug = args.debug
-
-    return args
+    global APP_STAGE
+    global APP_DEBUG
+    global APP_API_PORT
+    
+    APP_STAGE = args.stage
+    APP_DEBUG = args.debug
+    APP_API_PORT = args.port
